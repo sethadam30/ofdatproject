f64304e43cd165ebc19b10fced996b366186229b
Christophe Van Neste <Christophe.VanNeste@UGent.be>
2015-03-11 14:45:15 +0100

    first unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_sign_up(self):
+        #When a user arrives at the OFAT homepage the following information
+        #should be available: OFAT in title, signup/login, tabs for about,
+        #contact
+
+        self.browser.get('http://localhost:8000')
+        self.assertIn('OFAT', self.browser.title)
+        self.fail('Finish the test!')
+        
+        #The user is not yet registered and signs up
+
+        #After signing up and logging in it no longer displays the
+        #informational login, but the standard webapp layout for logged in
+        #users
+
+        #After signing up and having a first view of the full web app the user
+        #quits OFAT
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
