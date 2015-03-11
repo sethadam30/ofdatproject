70ed78014704d624f48c327ef667c870bf9a370e
Christophe Van Neste <Christophe.VanNeste@UGent.be>
2015-03-11 16:23:00 +0100

    first unit test and url mapping, dummy view

--- ofat/ofat/urls.py
+++ ofat/ofat/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ofat.views.home', name='home'),
+    url(r'^$', 'main.views.homepage', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
