13a95ba090b9ccdbb6ee6612a9d1aa94f7e3075e
Christophe Van Neste <Christophe.VanNeste@UGent.be>
2015-08-18 11:36:38 +0200

    Changing forum to scrollback.io pybbm dependencies can be dropped 	modified:   ofdat/ofdat/urls.py 	modified:   ofdat/templates/base.html 	new file:   ofdat/main/templates/main/forum.html

--- ofdat/ofdat/urls.py
+++ ofdat/ofdat/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'main.views.homepage', name='home'),
+    url(r'^forum$', TemplateView.as_view(template_name='main/forum.html'), name='forum'),
     # url(r'^blog/', include('blog.urls')),
 
-    (r'^forum/', include('pybb.urls', namespace='pybb')),
+    #(r'^forum/', include('pybb.urls', namespace='pybb')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout),
